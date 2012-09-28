cd4c4c491e0fa7c2bc55d4c3921e96e0f6c3d68a
thehurricane <ckmccutcheon@gmail.com>
2012-09-27 21:28:35 -0700

    init

--- web.js
+++ web.js
@@ -1,9 +1,21 @@
 var express = require('express');
 
+var sys = require("sys"),
+http = require("http"),  
+    url = require("url"),  
+    path = require("path"),  
+    fs = require("fs");  
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('./index.html');
+	fs.readFile('./index.html', function (err, data) {
+	if (err) throw err;
+		response.writeHead(200);
+		response.write(data);
+		response.end();
+	});
+	//response.send("Hello World");
 });
 
 var port = process.env.PORT || 5000;
